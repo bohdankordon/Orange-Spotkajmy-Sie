f4f4dad92fd9c3d8cbe4487344f716a0cfe1f0e3
Bohdan Kordon <bohdankordon@gmail.com>
2022-09-07 23:16:46 +0200

    add data, class Calendar and Meeting

--- src/main/java/Calendar.java
+++ src/main/java/Calendar.java
@@ -1,5 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Calendar {
 
+    private Set<Meeting> meetings = new HashSet<>();
 
+    private void addMeeting(Meeting meeting) {
+        meetings.add(meeting);
+    }
 
 }
--- src/main/java/Meeting.java
+++ src/main/java/Meeting.java
@@ -1,5 +1,29 @@
+import java.time.LocalTime;
+
 public class Meeting {
 
+    private LocalTime startTime;
+    private LocalTime endTime;
 
+    public Meeting(LocalTime startTime, LocalTime endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
 
 }
